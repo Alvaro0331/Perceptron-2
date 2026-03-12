23ae23dd90c4a5a4f12cd52ecc7516387e013aa2
Alvaro Davila Gomez <alvaritodavilag0331@gmail.com>
2026-03-12 00:26:54 -0600

    UI anima el proceso de entrenamiento

--- UI.py
+++ UI.py
@@ -1,6 +1,9 @@
 import matplotlib.pyplot as plt
 import matplotlib.widgets as widgets
 import numpy as np
+from matplotlib.backend_bases import MouseButton
+from perceptron import train_perceptron
+
 
 # Creacion de la figura
 def crear_figura():
@@ -15,13 +18,16 @@ def crear_figura():
     #Dibujo de ejes
     ax.axhline(y=0, color='black', linewidth=0.7)
     ax.axvline(x=0, color='black', linewidth=0.7)
+    global stateText
+    stateText = ax.text(0.02, 0.95, "", transform=ax.transAxes)
+
     
     """ #Leyenda
     leyenda = [
         plt.Line2D([0], [0], marker='o', color='w', label='Clase 0', markerfacecolor='blue', markersize=8),
         plt.Line2D([0], [0], marker='o', color='w', label='Clase 1', markerfacecolor='red', markersize=8),
         plt.Line2D([0], [0], marker='o', color='w', markerfacecolor='black', markersize=8, label='Sin clasificar'),
-        plt.Line2D([0], [0], linestyle='--', color='green', label='Frontera'),
+        plt.Line2D([0], [0], linestyle='-', color='green', label='Frontera'),
     ]
     ax.legend(handles=leyenda, loc='upper right', fontsize=8) """
     return fig, ax
@@ -29,7 +35,7 @@ def crear_figura():
 #Creacion de los widgets
 def crear_widgets(fig):
     # Botones
-    plotButton=widgets.Button(plt.axes([0.75, 0.3, 0.1, 0.15]), 'Plot', color='lightblue', hovercolor='skyblue')
+    plotButton=widgets.Button(plt.axes([0.75, 0.3, 0.1, 0.15]), 'Train', color='lightblue', hovercolor='skyblue')
     stopButton=widgets.Button(plt.axes([0.75, 0.2, 0.1, 0.05]), 'Stop', color='lightyellow', hovercolor='yellow')
     clearButton=widgets.Button(plt.axes([0.75, 0.1, 0.1, 0.05]), 'Clear', color='lightcoral', hovercolor='salmon')
     
@@ -39,20 +45,33 @@ def crear_widgets(fig):
 puntos = []  # Lista para almacenar los puntos clickeados
 markers = [] # Lista para almacenar los objetos de los puntos dibujados
 lineas = [] # Lista para almacenar la línea dibujada
+etiquetas = [] # Lista para almacenar las etiquetas de los puntos
 def onclick(event):
     if event.inaxes == ax:
-        x, y = event.xdata, event.ydata
-        marker, = ax.plot(x, y, 'ko')  # Dibuja un punto negro en la posición clickeada
-        puntos.append((x, y))  # Agrega el punto a la lista de puntos
-        markers.append(marker)  # Agrega el objeto del punto a la lista de markers
-        fig.canvas.draw()  # Actualiza la figura para mostrar el nuevo punto
+        if event.button is MouseButton.LEFT:
+            x, y = event.xdata, event.ydata
+            marker, = ax.plot(x, y, 'bo')  # Dibuja un punto azul en la posición clickeada
+            puntos.append((x, y))  # Agrega el punto a la lista de puntos
+            markers.append(marker)  # Agrega el objeto del punto a la lista de markers
+            etiquetas.append(0)  # Agrega la etiqueta correspondiente a la lista de etiquetas
+            fig.canvas.draw()  # Actualiza la figura para mostrar el nuevo punto
+        elif event.button is MouseButton.RIGHT:
+            x, y = event.xdata, event.ydata
+            marker, = ax.plot(x, y, 'ro')  # Dibuja un punto rojo en la posición clickeada
+            puntos.append((x, y))  # Agrega el punto a la lista de puntos
+            markers.append(marker)  # Agrega el objeto del punto a la lista de markers
+            etiquetas.append(1)  # Agrega la etiqueta correspondiente a la lista de etiquetas
+            fig.canvas.draw()  # Actualiza la figura para mostrar el nuevo punto
 
 
 #Evento para limpiar los puntos
 def clear(event):
     for marker in markers:
         marker.remove()  # Elimina el punto de la figura
+    for etiqueta in etiquetas:
+        etiquetas.remove(etiqueta)  # Elimina la etiqueta de la lista de etiquetas
     markers.clear()  # Reinicia la lista de markers
+    etiquetas.clear()
     puntos.clear() # Reinicia la lista de puntos
     clear_line()  # Limpia la línea de decisión
 
@@ -63,8 +82,45 @@ def clear_line():
     lineas.clear()  # Reinicia la lista de líneas
     fig.canvas.draw()  # Actualiza la figura para reflejar los cambios
 
+#Funcion para ejecutar el entrenamiento
+def train(event):
+    x=np.array(puntos, dtype=float)
+    d=np.array(etiquetas, dtype=int)
+    if len(x) < 2:
+        return
+    
+    w,bias,historial = train_perceptron(x,d)
+    for epoch, w_epoca, b_epoca in historial:
+        clear_line()
+        m,c,x_vertical=calcular(w_epoca[0], w_epoca[1], b_epoca)
+        stateText.set_text(f'Epoch: {epoch}')
+
+        if x_vertical is not None:
+            linea = ax.axvline(x=x_vertical, color='g', linewidth=1.5)
+            lineas.append(linea)
+        elif m is not None:
+            x_vals=np.linspace(-10, 10, 200)
+            y_vals=m*x_vals+c
+            linea, = ax.plot(x_vals, y_vals, 'g-', linewidth=1.5,)  # Dibuja la línea de decisión
+            lineas.append(linea)
+        fig.canvas.draw_idle()  # Actualiza la figura para mostrar los cambios
+        plt.pause(0.5)  # Pausa para visualizar el proceso de entrenamiento
+
+#Funcion para calcular m y c
+def calcular(w0,w1,bias):
+    if np.isclose(w1, 0.0):
+        if np.isclose(w0, 0.0):
+            return None, None, None
+        x_vertical = -bias / w0
+        return None, None, x_vertical
+    m = -w0 / w1
+    c = -bias / w1
+    return m, c, None
+
+
 fig, ax = crear_figura()
 plotButton, clearButton, stopButton = crear_widgets(fig)
 fig.canvas.mpl_connect('button_press_event', onclick)
 clearButton.on_clicked(clear)
+plotButton.on_clicked(train)
 plt.show()
--- perceptron.py
+++ perceptron.py
@@ -1,9 +1,6 @@
 import numpy as np
 
-def train_perceptron():
-    x= np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-    d= np.array([0, 0, 0, 1])
-
+def train_perceptron(x,d):
     # Inicialización de pesos
     n_features = x.shape[1]
     bias=np.random.uniform(0,1)
@@ -11,8 +8,8 @@ def train_perceptron():
 
     # Parámetros
     learning_rate = 0.01
-    epoch=0
-
+    epoch=1
+    historial = []  # Lista para almacenar el historial de pesos y bias
     # Entrenamiento del perceptrón
     while True:
         global_error=False
@@ -25,11 +22,14 @@ def train_perceptron():
             #Actualizar los pesos y el bias
             w=w+learning_rate*e*x[i]
             bias=bias+learning_rate*e
-            print(f"Epoch: {epoch}, Sample: {i}, Weights: {w}, Bias: {bias}, Error: {e}")
+            #print(f"Epoch: {epoch}, Sample: {i}, Weights: {w}, Bias: {bias}, Error: {e}")
             if e !=0:
                 global_error=True
+        historial.append((epoch,w.copy(), bias))  # Almacenar el historial de pesos y bias
         if not global_error:
-            print(f"Entrenamiento completado en {epoch} epochs.")
+            #print(f"Entrenamiento completado en {epoch} epochs.")
+            break
+        if epoch >= 100:
             break
         epoch += 1
     print("\nPredicciones finales: ")
@@ -44,6 +44,4 @@ def train_perceptron():
         if y == d[i]:
             correctos += 1
     print(f"Correctos: {correctos} de {len(x)}")
-
-
-train_perceptron()
+    return w, bias, historial
