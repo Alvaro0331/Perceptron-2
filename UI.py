3fb1bf8e42a75a82e683c3e4662da6a51f0887b2
Alvaro Davila Gomez <alvaritodavilag0331@gmail.com>
2026-03-11 21:31:07 -0600

    Funcion para limiar grafico

--- UI.py
+++ UI.py
@@ -38,7 +38,7 @@ def crear_widgets(fig):
 #Evento para agregar puntos con el mouse
 puntos = []  # Lista para almacenar los puntos clickeados
 markers = [] # Lista para almacenar los objetos de los puntos dibujados
-#lineas = [] # Lista para almacenar la línea dibujada
+lineas = [] # Lista para almacenar la línea dibujada
 def onclick(event):
     if event.inaxes == ax:
         x, y = event.xdata, event.ydata
@@ -48,7 +48,23 @@ def onclick(event):
         fig.canvas.draw()  # Actualiza la figura para mostrar el nuevo punto
 
 
+#Evento para limpiar los puntos
+def clear(event):
+    for marker in markers:
+        marker.remove()  # Elimina el punto de la figura
+    markers.clear()  # Reinicia la lista de markers
+    puntos.clear() # Reinicia la lista de puntos
+    clear_line()  # Limpia la línea de decisión
+
+#Funcion para limpiar la linea
+def clear_line():
+    for linea in lineas:
+        linea.remove()  # Elimina la línea de la figura
+    lineas.clear()  # Reinicia la lista de líneas
+    fig.canvas.draw()  # Actualiza la figura para reflejar los cambios
+
 fig, ax = crear_figura()
 plotButton, clearButton, stopButton = crear_widgets(fig)
 fig.canvas.mpl_connect('button_press_event', onclick)
+clearButton.on_clicked(clear)
 plt.show()
