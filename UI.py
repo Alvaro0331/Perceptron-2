a6537ffe8412a2de99a8a78534c1a57fe2872306
Alvaro Davila Gomez <alvaritodavilag0331@gmail.com>
2026-03-11 19:31:53 -0600

    Creada interfaz grafica para el perceptron

---
+++ UI.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+import matplotlib.widgets as widgets
+import numpy as np
+
+# Creacion de la figura
+def crear_figura():
+    fig = plt.figure(figsize=(10, 6))
+    ax = plt.axes([0.1, 0.1, 0.6, 0.8]) # [left, bottom, width, height]
+    ax.set_title("Perceptrón - Clasificación de puntos")
+    ax.set_xlim(-10, 10)
+    ax.set_ylim(-10, 10)
+    ax.grid(True, linestyle='--', alpha=0.5)
+    ax.set_xlabel("X2", fontsize=12)
+    ax.set_ylabel("X1", fontsize=12)
+    #Dibujo de ejes
+    ax.axhline(y=0, color='black', linewidth=0.7)
+    ax.axvline(x=0, color='black', linewidth=0.7)
+    
+    #Leyenda
+    leyenda = [
+        plt.Line2D([0], [0], marker='o', color='w', label='Clase 0', markerfacecolor='blue', markersize=8),
+        plt.Line2D([0], [0], marker='o', color='w', label='Clase 1', markerfacecolor='red', markersize=8),
+        plt.Line2D([0], [0], marker='o', color='w', markerfacecolor='black', markersize=8, label='Sin clasificar'),
+        plt.Line2D([0], [0], linestyle='--', color='green', label='Frontera'),
+    ]
+    ax.legend(handles=leyenda, loc='upper right', fontsize=8)
+    return fig, ax
+
+
+
+
+fig, ax = crear_figura()
+plt.show()
